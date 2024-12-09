f0720eb062aa63eeda2575f8b1a6c0c1768810a2
Ilya Tvaliashvili <itvaliaschvili@icloud.com>
2024-12-10 01:47:02 +0300

    fixed routing

--- src/App.tsx
+++ src/App.tsx
@@ -1,9 +1,22 @@
-import { Outlet } from "react-router";
+import { Outlet, useLocation, useNavigate } from "react-router";
 import styles from "./App.module.css";
 import { Header } from "./components/Header/Header";
 import { Footer } from "./components/Footer/Footer";
+import { useEffect } from "react";
+import { PATHS } from "./consts";
+
+const DEFAULT_ROUTE = PATHS.ADD_POST;
 
 function App() {
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (!Object.values(PATHS).includes(location.pathname)) {
+      navigate(DEFAULT_ROUTE);
+    }
+  }, [location.pathname, navigate]);
+
   return (
     <div className={styles.container}>
       <Header />
--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
 import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
-import { createBrowserRouter, Navigate, RouterProvider } from "react-router";
+import { createBrowserRouter, RouterProvider } from "react-router";
 import { AddPost } from "./pages/AddPost/AddPost";
 import App from "./App";
 import { PATHS } from "./consts";
-
-const DEFAULT_PAGE = "/add-post";
 
 const router = createBrowserRouter([
   {
@@ -16,16 +14,13 @@ const router = createBrowserRouter([
       {
         path: PATHS.ADD_POST,
         element: <AddPost />,
+        index: true,
       },
       {
         path: PATHS.ADD_PODCAST,
         element: null,
       },
     ],
-  },
-  {
-    path: "*",
-    element: <Navigate to={DEFAULT_PAGE} />,
   },
 ]);
 
